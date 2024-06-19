f69f0fc79f36079def94f5f690fe44db8bc3b955
evg9n <eea@i-tat.ru>
2024-06-19 17:46:22 +0300

    update

--- static/js/map.js
+++ static/js/map.js
@@ -1,7 +1,8 @@
-var width = parseFloat(document.getElementById('width').textContent);
-var longitude = parseFloat(document.getElementById('longitude').textContent);
+var width = parseFloat(document.getElementById('width').textContent.replace(',', '.'));
+var longitude = parseFloat(document.getElementById('longitude').textContent.replace(',', '.'));
 var name_label = document.getElementById('name_label').textContent;
-
+console.log(width);
+console.log(longitude);
 ymaps.ready(init);
 function init(){
 var myMap = new ymaps.Map("map", {
